7f4210f08ba9cd658c05786b152df1d8a3f09c2e
Gak250 <stopsigngladiators@gmail.com>
2015-03-15 16:38:57 -0700

    make turn functions more relyable

--- tortoise.lua
+++ tortoise.lua
@@ -14,37 +14,64 @@ position = {
 	orientation = direction.south,
    
 	turnLeft = function()
-		if turtle.turnLeft() then
-			if position.orientation == direction.north then
-				position.orientation = direction.west
-			else
-				position.orientation = position.orientation - 1
-			end
+		if not turtle.turnLeft() then
+			return false
 		end
+		
+		if position.orientation == direction.north then
+			position.orientation = direction.west
+		else
+			position.orientation = position.orientation - 1
+		end
+		return true
 	end,
 	
 	turnRight = function()
-		if turtle.turnRight() then
-			if position.orientation == direction.west then
-				position.orientation = direction.north
-			else
-				position.orientation = position.orientation + 1
+		if not turtle.turnRight() then
+			return false
+		end
+		
+		if position.orientation == direction.west then
+			position.orientation = direction.north
+		else
+			position.orientation = position.orientation + 1
+		end
+		return true
+	end,
+	
+	faceLeft = function(faceDirection)
+		while position.orientation ~= faceDirection do
+			if not position.turnLeft() then
+				return false
 			end
 		end
+		return true
+	end,
+	
+	faceRight = function(faceDirection)
+		while position.orientation ~= faceDirection do
+			if not position.turnRight() then
+				return false
+			end
+		end
+		return true
 	end,
 	
 	face = function(faceDirection)
-		if (position.orientation == direction.north and faceDirection == direction.west) or position.orientation - 1 == faceDirection then
-			position.turnLeft()
-			print "turnLeft"
-		else 
-			while position.orientation ~= faceDirection do
-				print "turnRight"
-				position.turnRight()
+		if (position.orientation == direction.north and faceDirection == direction.west) or 
+				position.orientation - 1 == faceDirection then
+				
+			if position.faceLeft(faceDirection) then
+				return true
 			end
+			return position.faceRight(faceDirection)
 		end
+		
+		if position.faceRight(faceDirection) then
+			return true
+		end
+		return position.faceLeft(faceDirection)			
 	end,
 }
-
 
 
